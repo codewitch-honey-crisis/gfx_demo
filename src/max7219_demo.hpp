0aa50eda25e79aad177a864d587d4cee75459780
codewitch-honey-crisis <gaztoast@gmail.com>
2021-05-22 11:36:13 -0700

    updated demo to potentially work with multiple rows of segments

--- src/max7219_demo.hpp
+++ src/max7219_demo.hpp
@@ -82,21 +82,19 @@ void app_main() {
         printf("Could not initialize driver.\r\n");
         vTaskDelay(portMAX_DELAY);
     }
-    const char* text = "ESP32 GFX Demo - MAX7219 **     ";
+    const char* text = "ESP32 GFX Demo - MAX7219 ** Copyright (C) 2021 by honey the codewitch   ";
     size_t len = strlen(text);
     using bmp_type = bitmap<typename matrix_type::pixel_type>;
     const font& f= Bm437_Acer_VGA_8x8_FON;
-    size16 bmp_size = (size16)f.measure_text({int16_t(len*f.average_width()),(int16_t)f.height()}, text);
+    size16 bmp_size = size16(f.measure_text({int16_t(len*f.average_width()),(int16_t)f.height()}, text).width,matrix.dimensions().height);
     uint8_t* bmp_buf = (uint8_t*)malloc(bmp_type::sizeof_buffer(bmp_size));
     bmp_type bmp(bmp_size,bmp_buf);
     bmp.clear(bmp.bounds());
-    draw::text(bmp,(srect16)bmp.bounds(),text,f,matrix_color::white);
-    print_source(bmp);   
+    int cy = (bmp.dimensions().height-f.height())/2;
+    draw::text(bmp,srect16(spoint16(0,cy),ssize16(bmp.dimensions().width,f.height())),text,f,matrix_color::white);
     int x = 0;
     while(true) {
-            draw::suspend(matrix);
             draw::bitmap(matrix,srect16(spoint16(0,0),(ssize16)matrix.dimensions()),bmp,rect16(point16(x,0),matrix.dimensions()));
-            draw::resume(matrix);
             vTaskDelay(50/portTICK_PERIOD_MS);
         	if( ++x > bmp.dimensions().width ) {
                 x = 0;
